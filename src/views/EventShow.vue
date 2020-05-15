c05b3365447150c17e658c05f2b832da97a09ec3
adetyaz <tayo.lasisi@yahoo.com>
2020-05-15 21:39:32 +0100

    initial commit

---
+++ src/views/EventShow.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Events Show {{ event.title }}</h1>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js';
+
+export default {
+  props: ['id'],
+
+  data() {
+    return {
+      event: {}
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(res => {
+        this.event = res.data;
+      })
+      .catch(err => {
+        console.log('This could not be loaded due to' + err);
+      });
+  }
+};
+</script>
