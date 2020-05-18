b0c52fdc6e1b457d0f1195e91d0e5f2d0e9866aa
adetyaz <tayo.lasisi@yahoo.com>
2020-05-18 23:18:16 +0100

    store update

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,16 @@
+
 <template>
-  <h1>Events Create</h1>
+  <div>
+    <h1>Event created by {{ user.name }}</h1>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>the are {{ getEventById(2) }} categories</p>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
