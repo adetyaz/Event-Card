8cb0a21b34ea8a68815a4644965726a3d057ec6b
adetyaz <tayo.lasisi@yahoo.com>
2020-06-25 15:48:26 +0100

    ..

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Uh-Oh</h1>
+    <p>
+      it seems you have encountered a network issue
+      <router-link :to="{ name: 'event-list' }" />G o back to the previous page
+    </p>
+  </div>
+</template>
+
